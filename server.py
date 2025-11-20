687908b19b2c50ab396abcf3b8f80c7f23eee3bc
Justin Tang Ly <justinly818@gmail.com>
2025-11-20 14:51:42 -0800

    fixed crossrange issue with negative roll changes not reflecting properly

--- server.py
+++ server.py
@@ -560,6 +560,8 @@ class HypersonicGlideSimulator:
             cosa = (np.sin(psi))**2 + np.cos(2*omega) * (np.cos(psi))**2
             cosa = np.clip(cosa, -1.0, 1.0)
             crange_earth = 0.5 * self.Rearth * np.arccos(cosa)
+            if roll < 0:
+                crange_earth = crange_earth * -1
             
             sine = np.cos(omega) * np.cos(psi) / ((1+cosa)/2)**0.5
             sine = np.clip(sine, -1.0, 1.0)
